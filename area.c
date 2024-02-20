c685cc502c8c7c909fec9c61ad6e34ccef8d0f9d
Priyagita16 <148611002+Priyagita16@users.noreply.github.com>
2024-02-20 15:18:34 +0530

    Update area.c

--- area.c
+++ area.c
@@ -4,13 +4,8 @@
  
 int main()
 {
- 
-<<<<<<< HEAD
-    int l = 30, b = 50;
-=======
-    int l = 20, b = 10;
->>>>>>> 2907eac0cc7329997a878e59fa18011723fd35d8
 
+    int l = 30, b = 50;
     printf("Area of rectangle is : %d", l * b);
     printf("\nPerimeter of rectangle is : %d", 2 * (l + b));
     return 0;
