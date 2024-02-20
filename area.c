eb8c42b3f8ec8f6aec12cf1834b238d57baad01f
bhanushreer <mrbhanushree@gmail.com>
2024-02-20 15:11:10 +0530

    commit

--- area.c
+++ area.c
@@ -5,7 +5,7 @@
 int main()
 {
  
-    int l = 10, b = 10;
+    int l = 30, b = 10;
     printf("Area of rectangle is : %d", l * b);
     printf("\nPerimeter of rectangle is : %d", 2 * (l + b));
     return 0;
